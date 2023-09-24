de0701719822939659e84fd5171513301bde3b08
marcin10lw <marcin10lw@wp.pl>
2023-09-24 22:03:51 +0200

    setup placeholder CRUD

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,19 +1,81 @@
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end dev" },
+  { id: nanoid(), company: "google", position: "back-end dev" },
+];
+
 export const getAllJobs = async (req, res) => {
-  res.send("all jobs");
+  res.status(200).json({ jobs });
 };
 
 export const getSingleJob = async (req, res) => {
-  res.send("single job");
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ job });
+  }
+
+  res.status(200).json({ job });
 };
 
 export const createJob = async (req, res) => {
-  res.send("created job");
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Must provide company and position" });
+  }
+
+  const newJob = { id: nanoid(), company, position };
+
+  jobs = [...jobs, newJob];
+  res.status(201).json({ job: newJob });
 };
 
 export const updateJob = async (req, res) => {
-  res.send("updated job");
+  const { id } = req.params;
+  const { company, position } = req.body;
+
+  if (!company && !position) {
+    return res.status(400).json({ msg: `Must provide company or position` });
+  }
+
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No jobs with id: ${id}` });
+  }
+
+  const updateJob = {
+    ...job,
+    company: company || job.company,
+    position: position || job.position,
+  };
+
+  jobs = jobs.map((job) => {
+    if (job.id === id) {
+      return {
+        ...job,
+        ...updateJob,
+      };
+    }
+
+    return job;
+  });
+
+  res.status(200).json({ job: updateJob });
 };
 
 export const deleteJob = async (req, res) => {
-  res.send("deleted job");
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No jobs with id: ${id}` });
+  }
+
+  jobs = jobs.filter((job) => job.id !== id);
+
+  res.status(200).json({ msg: `Deleted job with id: ${id}` });
 };
