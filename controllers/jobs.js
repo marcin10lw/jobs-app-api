6c016ec43103f41d10dc85950f0be4b94ea85196
marcin10lw <marcin10lw@wp.pl>
2023-09-24 21:35:02 +0200

    created placeholder jobs controllers

---
+++ controllers/jobs.js
@@ -0,0 +1,19 @@
+export const getAllJobs = async (req, res) => {
+  res.send("all jobs");
+};
+
+export const getSingleJob = async (req, res) => {
+  res.send("single job");
+};
+
+export const createJob = async (req, res) => {
+  res.send("created job");
+};
+
+export const updateJob = async (req, res) => {
+  res.send("updated job");
+};
+
+export const deleteJob = async (req, res) => {
+  res.send("deleted job");
+};
