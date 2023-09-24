1855ebd7f1f3ed811a2bcb5a42fe11ebef55fdcb
marcin10lw <marcin10lw@wp.pl>
2023-09-24 23:55:34 +0200

    created custom, bad request, not found errors

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+import CustomError from "./custom-error";
+import BadRequestError from "./bar-request-error";
+import NotFoundError from "./not-found-error";
+
+export default { CustomError, BadRequestError, NotFoundError };
