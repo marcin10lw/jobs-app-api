6dc70a7b1a3d3ecfc6029a8fbb3ea72615c5b094
marcin10lw <marcin10lw@wp.pl>
2023-09-24 21:02:14 +0200

    installed and setup morgan dev mode

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/v1/jobs", (req, res) => {
   res.send("Hello from Homepage");
+});
+
+app.post("/", (req, res) => {
+  const body = req.body;
+
+  res.status(200).json(body);
 });
 
 const port = process.env.PORT || 3000;
