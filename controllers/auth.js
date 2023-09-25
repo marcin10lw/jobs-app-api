67d081102ea04bb020b7cdf245cb6b0f9e0ea366
marcin10lw <marcin10lw@wp.pl>
2023-09-25 22:44:55 +0200

    created logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,3 +35,12 @@ export const login = async (req, res) => {
   });
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user logged out" });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.js";
+import { register, login, logout } from "../controllers/auth.js";
 import { validateLogin, validateRegister } from "../middleware/validation.js";
 
 const router = Router();
 
 router.post("/register", validateRegister, register);
 router.post("/login", validateLogin, login);
+router.get("/logout", logout);
 
 export default router;
